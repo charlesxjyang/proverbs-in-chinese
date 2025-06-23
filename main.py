5c0ef69fa038369cc0cca763b796a047c7c79ff3
Charles Yang <charlesxjyang@gmail.com>
2025-06-22 23:31:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import json
 ESV_API_KEY = os.environ['ESV_API_KEY']
 EMAIL_SENDER = os.environ['EMAIL_SENDER']
 EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
-EMAIL_RECEIVER = os.environ['EMAIL_RECEIVER']
+EMAIL_RECEIVER = json.loads(os.environ['EMAIL_RECEIVER'])
 
 
 def get_random_proverb():
